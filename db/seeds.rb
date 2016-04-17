7b10db1479fad44c1b5edb2a2e277317a750b914
mostafa <mostafa.k.zahran@gmail.com>
2016-04-17 02:57:12 +0200

    Fill data in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name: 'Administrator', admin: true, email: 'admin@sinterest.com')
+admin_user = User.create(name: 'Administrator', admin: true, email: 'admin@sinterest.com', password: '123456789', password_confirmation: '123456789')
+normal_user = User.create(name: 'Test User', admin: false, email: 'test_user1@sinterest.com', password: '123456789', password_confirmation: '123456789')
+track_file = Rails.root.join('spec/dog_puppy.wav').open
+admin_track = Track.create(name: 'Track1', user: admin_user, sound_track: track_file)
+normal_track = Track.create(name: 'Track2', user: normal_user, sound_track: track_file)
+admin_playlist = Playlist.create(name: 'Playlist1', user: admin_user, tracks: [admin_track])
+normal_playlist = Playlist.create(name: 'Playlist2', user: normal_user, tracks: [normal_track])
